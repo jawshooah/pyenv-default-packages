501dcd3329aeb321d92d8adebee034bf6272be09
Josh Hagins <hagins.josh@gmail.com>
2015-03-26 16:47:16 -0400

    Extract common code to libexec/default-packages.sh

--- etc/pyenv.d/install/default-packages.bash
+++ etc/pyenv.d/install/default-packages.bash
@@ -1,21 +1,8 @@
+#!/bin/env bash
+source "$(dirname ${BASH_SOURCE[0]})/../../../libexec/default-packages.sh"
+
 if declare -Ff after_install >/dev/null; then
-  after_install install_default_packages
+  after_install "install_default_packages $VERSION_NAME"
 else
   echo "pyenv: pyenv-default-packages plugin requires pyenv v0.1.0 or later" >&2
 fi
-
-install_default_packages() {
-  # Only install default packages after successfully installing Python.
-  [ "$STATUS" = "0" ] || return 0
-
-  local requirements_file=${PYENV_ROOT}/default-packages
-
-  if [ -f "$requirements_file" ]; then
-    local args=( -r "$requirements_file" )
-
-    # Invoke `pip install` in the just-installed Python.
-    PYENV_VERSION="$VERSION_NAME" pyenv-exec pip install "${args[@]}" || {
-      echo "pyenv: error installing packages from  \`$requirements_file'"
-    } >&2
-  fi
-}
