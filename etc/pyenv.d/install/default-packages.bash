b78465fc2b5436b0ecdbc8982377adffa8b8520b
Josh Hagins <hagins.josh@gmail.com>
2015-03-27 02:31:16 -0400

    Fix install hook
    VERSION_NAME is not set before the installer runs, so don't try to
    access its value outside of the install_default_packages function.

--- etc/pyenv.d/install/default-packages.bash
+++ etc/pyenv.d/install/default-packages.bash
@@ -2,7 +2,7 @@
 source "${BASH_SOURCE[0]%/*}/../../../libexec/default-packages.sh"
 
 if declare -Ff after_install >/dev/null; then
-  after_install "install_default_packages $VERSION_NAME"
+  after_install 'install_default_packages $VERSION_NAME'
 else
   echo "pyenv: pyenv-default-packages plugin requires pyenv v0.1.0 or later" >&2
 fi
--- etc/pyenv.d/virtualenv/default-packages.bash
+++ etc/pyenv.d/virtualenv/default-packages.bash
@@ -2,7 +2,7 @@
 source "${BASH_SOURCE[0]%/*}/../../../libexec/default-packages.sh"
 
 if declare -Ff after_virtualenv >/dev/null; then
-  after_virtualenv "install_default_packages $VIRTUALENV_NAME"
+  after_virtualenv 'install_default_packages $VIRTUALENV_NAME'
 else
   echo "pyenv: pyenv-default-packages plugin requires pyenv-virtualenv v20130527 or later" >&2
 fi
